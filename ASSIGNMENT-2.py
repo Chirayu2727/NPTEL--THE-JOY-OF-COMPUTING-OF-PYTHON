2b4803f1935e60f469be6fdc4505cd248b9517af
Chirayu2727 <128579419+Chirayu2727@users.noreply.github.com>
2023-08-10 00:38:05 +0530

    Add files via upload
    assignment -2 solution

---
+++ ASSIGNMENT-2.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#----------------------------------------- ---ASSIGNMENT-2-----------------------------------------------------------------------
+
+#q1 which of the following is a high-level programming language?
+#ans----->python
+
+
+# In[2]:
+
+
+#Q2WHICH OF the following is an example of a front-end programming language?
+#ans------------>HTML
+
+
+# In[3]:
+
+
+#Q3 WHIch is the following is used to store data in a programming language?
+#ans3----------->variables & array
+
+
+# In[5]:
+
+
+#q4
+# ans4----------------->
+print('the joy of computing')
+
+
+# In[9]:
+
+
+#q5
+#ans--->11
+for i in range(11):
+ print(i)
+
+
+# In[11]:
+
+
+#q6
+ans-----> def
+
+
+# In[15]:
+
+
+#q7
+# ans----->b=45
+b=0
+for i in range(10):
+    b=b+i
+    
+    print(b)
+
+
+# In[17]:
+
+
+#q8
+
+#c=b**3
+#ans------> it will throw an error
+c=b**3
+print(c)
+
+
+# In[20]:
+
+
+#q9
+
+
+#ans-------->3030
+number=input("enter number:")
+new_number=number*2
+print(new_number)
+
+
+# In[19]:
+
+
+#q10
+#ans-------->false
+a=3
+b=4
+c=a>b
+print(c)
+
+
+# In[ ]:
+
+
+#you will passed the assignment criteria with flying colors
+#--------------------------------------------------------------
+# https://github.com/Chirayu2727
+#stay tunned for assignment -3
+
