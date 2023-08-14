0c9955c1b1be6e33daa43c55de2c7a4ce2dabd5d
Chirayu2727 <128579419+Chirayu2727@users.noreply.github.com>
2023-08-14 20:46:16 +0530

    Add files via upload

---
+++ _ASSIGNMENT --03 (THE -JOY- OF -COMPUTING -IN -PYTHON).py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# q1-->
+
+l=["a","b","c","d","e","f","g","h"]
+print(l[2:5])
+#ans --> c d e
+
+
+# In[4]:
+
+
+#q2
+ #ans---->
+d={1:"one",2:"two",3:"three"}
+print(d)
+
+
+# In[ ]:
+
+
+#q3
+#insert() used for specific index
+#append() will add at the end of list
+ans---> insert()
+
+
+# In[6]:
+
+
+#q4
+ # to add something at the end we use append()
+    
+l.append("s")
+print(l)
+
+
+# In[ ]:
+
+
+#q5 
+
+ans---> char
+
+
+# In[8]:
+
+
+#q6
+for i in range(1,21):
+        if i%3==0 and i%5==0:
+            print("fizzbuss")
+        elif i%3==0:
+            print("fizz")
+        elif i%5==0:
+            print("buzz")
+        else:
+            print(i)
+            # ans--> print fizz for multiple of 3 and buzz for multiple of 5
+
+
+# In[9]:
+
+
+#q7
+a=5
+b=2
+print(a//b)
+ans--> 2
+
+
+# In[11]:
+
+
+#q8
+s="hello"
+print(s[::-1])  # this is the right answer 
+print(s[:-1])
+
+
+# In[12]:
+
+
+#q9
+a=10
+b=5
+c=a%b 
+print(c)
+#ans--> 0
+
+
+# In[13]:
+
+
+#Q10
+s="python"
+print(s[1:4])
+#ans---->"yth"
+
+
+# In[ ]:
+
+
+
+
